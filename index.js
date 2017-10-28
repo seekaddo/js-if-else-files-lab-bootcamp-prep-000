11caf06755a27c6bc191c31fd237b3ac545ff3a7
Dennis Addo <dennis_addo@aol.com>
2017-10-28 18:17:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var name = "Joe"
 var height = "74"
 
-if (name === "Susan") {
-  console.log("The Name is correct");
-}else {
-  console.error("The name is not the same as Susan");
-}
+// if (name === "Susan") {
+//   console.log("The Name is correct");
+// }else {
+//   console.error("The name is not the same as Susan");
+// }
 
 // Don't worry about this
 module.exports = { name, height
